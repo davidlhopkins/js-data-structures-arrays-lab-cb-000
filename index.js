c2243fed6f03cd530f89218e5b0f5a83d2556a92
David Hopkins <dlhopkins2@gmail.com>
2018-07-15 06:07:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver() {
   drivers.push('Ralph');
 }
+
+function destructivelyAppendDriver() {
+  drivers.unshift('Ralph');
+}
